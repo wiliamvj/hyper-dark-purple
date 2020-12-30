d25f5db599bd1665e6de76bf23ac83ae4512683a
wiliamvj <wiliamjoaquim@gmail.com>
2020-12-30 13:28:53 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# Theme Dark Purple for [Hyper](https://hyper.is)
+
+![Screenshot](./screenshot.png)
+
+## [Install](./INSTALL.md)
+
+#### Install using config file
+
+Clone this repo to `~/.hyper_plugins/local/`
+
+Add `theme-launchbase-hyper` to the localPlugins list in your `~/.hyper.js` config file.
+
+```sh
+    localPlugins: [
+    	'hyper-dark-purple'
+    ]
+```
+
+#### Activating theme
+
+1.  Start Hyper
+2.  Go to `View -> Full Reload` or pressing `Cmd + Shft + R`
+
+## License
+
+MIT License
--- main.js
+++ main.js
@@ -1,53 +1,49 @@
 'use strict';
 
-const backgroundColor = '#0c021d';
-const foregroundColor = '#fcfcf2';
-const borderColor = '#9056eb';
-const cursorColor = '#9056eb';
-const borderRadius = '5px';
+const backgroundColor = '#10002b';
+const foregroundColor = '#5a189a';
+const borderColor = '#7b2cbf';
+const cursorColor = '#7b2cbf';
+const selectionColor = '#7b2cbf';
 const colors = {
   black: '#000000',
-  red: '#d10000',
-  green: '#4cd137',
-  yellow: '#8c7ae6',
-  blue: '#00638a',
-  magenta: '#8c7ae6', // old: #dc3091
-  cyan: '#9056eb',
+  red: '#ff5555',
+  green: '#50fa7b',
+  yellow: '#f1fa8c',
+  blue: '#bd93f9',
+  magenta: '#ff79c6',
+  cyan: '#8be9fd',
   white: '#bfbfbf',
-  lightBlack: '#000000aa',
-  lightRed: '#d10000',
-  lightGreen: '#4cd137',
-  lightYellow: '#8c7ae6',
-  lightBlue: '#00638a',
-  lightMagenta: '#8c7ae6',
-  lightCyan: '#9056eb',
-  lightWhite: '#bfbfbf'
+  lightBlack: '#4d4d4d',
+  lightRed: '#ff6e67',
+  lightGreen: '#5af78e',
+  lightYellow: '#f4f99d',
+  lightBlue: '#9d4edd',
+  lightMagenta: '#ff92d0',
+  lightCyan: '#9aedfe',
+  lightWhite: '#e6e6e6',
 };
 
-exports.decorateConfig = config => {
+exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
     backgroundColor,
     foregroundColor,
     borderColor,
     cursorColor,
+    selectionColor,
     colors,
-    borderRadius,
     termCSS: `
       ${config.termCSS || ''}
     `,
     css: `
       ${config.css || ''}
-      .hyper_main {
-        border-radius: 6px;
-      }
-			.tabs_list .tab_tab.tab_active .tab_text  {
+      .tabs_list .tab_tab.tab_active .tab_text  {
         background: ${backgroundColor};
-        border-radius: ${borderRadius};
-			}
-      
-			.tab_active:before {
+      }
+
+      .tab_active:before {
         border-color: ${borderColor};
-        border-radius: ${borderRadius};
-		`
+      }
+    `,
   });
 };
